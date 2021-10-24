1568b5f3bc76d64b14603b71b86b9602e8d799ce
KRyun <hihas93@naver.com>
2021-10-24 18:07:01 +0900

    skill Page animation & Info 완성

---
+++ src/components/SkillBar.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const SkillBarBlock = styled.div`
+  .info {
+    padding-bottom: 1.5rem;
+  }
+
+  .info span{
+    font-size: 1.5rem;
+    font-weight: 500;
+    opacity: 0;
+    animation: showText 0.5s 1s linear forwards;
+  }
+
+  @keyframes showText {
+    100%{
+      opacity: 1;
+    }
+  }
+
+  .progressLine {
+    position: relative;
+    height: 10px;
+    width: 100%;
+    background-color: #f0f0f0;
+    transform-origin: left;
+    transform: scaleX(0);
+    box-shadow: inset 0px 1px 1px rgba(0,0,0,0.05),
+                      0px 1px rgba(255,255,255,0.8);
+    animation: animate 1s cubic-bezier(1,0,0.5,1) forwards;
+  }
+
+  @keyframes animate {
+    100% {
+      transform: scaleX(1);
+    }
+  }
+
+  .progressLine span {
+    height: 100%;
+    width: ${props=>props.percent || 0};
+    background: #6665ee;
+    position: absolute;
+    border-radius: 10px;
+    transform-origin: left;
+    transform: scaleX(0);
+    animation: animate 1s 1s cubic-bezier(1,0,0.5,1) forwards;
+  }
+
+  .progressLine span::before {
+    position: absolute;
+    content: "";
+    right: 0;
+    top: -10px;
+    height: 0;
+    width: 0;
+    border: 7px solid transparent;
+    border-bottom-width: 0px;
+    border-right-width: 0px;
+    border-top-color: #000;
+    opacity: 0;
+    animation: showText2 0.5s 1.5s linear forwards;
+  }
+
+  .progressLine span::after {
+    position: absolute;
+    content: "${props=> props.percent}";
+    right: 0;
+    top: -27px;
+    color: #fff;
+    font-size: 12px;
+    font-weight: 500;
+    background: #000;
+    padding: 1px 8px;
+    border-radius: 3px;
+    opacity: 0;
+    animation: showText2 0.5s 1.5s linear forwards;
+  }
+
+  @keyframes showText2 {
+    100%{
+      opacity: 1;
+    }
+
+  }
+`;
+
+const SkillBar = ({skillName, percent})=>{
+  return(
+    <SkillBarBlock percent={percent} skillName={skillName}>
+      <div className="info">
+        <span>{skillName}</span>
+      </div>
+      <div className="progressLine">
+        <span></span>
+      </div>
+    </SkillBarBlock>
+  )
+};
+
+SkillBar.propTypes = {
+  percent: PropTypes.string,
+}
+
+export default SkillBar;
--- src/components/SkillSet.js
+++ src/components/SkillSet.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import SkillBar from './SkillBar';
+import { AiOutlineQuestionCircle } from "react-icons/ai";
 
 const SkillSetBlock = styled.div`
   padding: 2rem;
@@ -8,114 +9,193 @@ const SkillSetBlock = styled.div`
   width: 100%;
 
   .header {
-    margin: 2rem 1rem 4rem;
+    margin: 3rem 1rem 7rem;
     font-size: 2rem;
     font-weight: 600;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 
-  .header span {
+  .header .skillSetTitle {
     padding-left: 2rem;
     border-left: solid 0.8rem black;
   }
+
+  .header .skillBarInfo {
+    position: relative;
+
+    .skillBarInfoDetail {
+      display: none;
+    }
+
+    &:hover .skillBarInfoDetail{
+      display: block;
+      position: absolute;
+      top: -4.3rem;
+      font-size: 0.5rem;
+      background-color: #757575;
+      color: white;
+      width: 70%;
+      left: 10%;
+      padding: 8px;
+      border-radius: 6px;
+
+      & > div{
+        margin-bottom: 2px;
+      }
+      & > div:last-child {
+        margin: 0;
+      }
+      &::after {
+        position: absolute;
+        content:"";
+        top: 4.8rem;
+        left: 60%;
+        width: 0;
+        height: 0;
+        border: 9px solid transparent;
+        border-bottom-width: 0px;
+        border-right-width: 0px;
+        border-top-color: #757575;
+      }
+
+    }
+  }
+
+
+  .header .skillBarInfo .gridContainer {
+  
+    display: inline-grid;
+    grid-template-columns: 2fr 2fr 2fr 2fr 1fr;
+    grid-template-rows: 0.5fr 1fr 1fr;
+    font-size: 0.5rem;
+    margin: 0 2rem;
+    grid-gap: 4px 0;
+
+    & > div {
+      padding: 0 0.8rem;
+      text-align: center;
+    }
+
+
+    & > div:first-child {
+      border: none;
+      border-radius: 1rem;
+      grid-column: 1/5;
+      background-color: black;
+      position: relative;
+      width: 100%;
+      background-color: #f0f0f0;
+      
+      & > span {
+        height: 100%;
+        width: 20%;
+        background: #6665ee;
+        position: absolute;
+        border-radius: 10px;
+        animation-name: progressLoop;
+        animation-duration: 2s;
+        animation-iteration-count: infinite;
+        animation-timing-function: ease-in-out;
+        animation-direction: alternate;
+      }
+
+      @keyframes progressLoop {
+        from {
+          left: 0%;
+        }
+        to {
+          left: 80%;
+        }
+      }
+      
+    }
+
+    & > div:last-child {
+      border: none;
+      grid-column: 5/6;
+      grid-row: 1/4;
+      display: flex;
+      align-items: center;
+      font-size: 1rem;
+    }
+
+
+  }
+
   .container {
     display: flex;
     justify-content: space-between;
-    
+
     div {
       flex: 1 1 0;
       margin: 1rem;
     };
+
+    & > div {
+      align-self: flex-start;
+      position:relative;
+      border: 3px solid black;
+      border-top: none;
+      border-radius: 1.5rem;
+      padding: 2rem 0;
+      padding-right: 2rem;
+      & > span {
+        position: absolute;
+        top: -2rem;
+        display: block;
+        font-size: 2rem;
+        font-weight: 500;
+        padding-left: 3rem;
+        padding-bottom: 2rem;
+      }
+    }
   };
 `;
-
-const SkillBarBlock = styled.div`
-
-  .info {
-    padding-bottom: 1.5rem;
-  }
-
-  .info span{
-    font-size: 1.5rem;
-    font-weight: 500;
-  }
-
-  .progressLine {
-    position: relative;
-    height: 10px;
-    width: 100%;
-    background-color: #f0f0f0;
-    box-shadow: inset 0px 1px 1px rgba(0,0,0,0.05),
-                      0px 1px rgba(255,255,255,0.8);
-  }
-
-  .progressLine span {
-    height: 100%;
-    width: ${props=>props.percent || 0};
-    background: #6665ee;
-    position: absolute;
-    border-radius: 10px;
-  }
-
-  .progressLine span::before {
-    position: absolute;
-    content: "";
-    right: 0;
-    top: -10px;
-    height: 0;
-    width: 0;
-    border: 7px solid transparent;
-    border-bottom-width: 0px;
-    border-right-width: 0px;
-    border-top-color: #000;
-  }
-
-  .progressLine span::after {
-    position: absolute;
-    content: "${props=> props.percent}";
-    right: 0;
-    top: -27px;
-    color: #fff;
-    font-size: 12px;
-    font-weight: 500;
-    background: #000;
-    padding: 1px 8px;
-    border-radius: 3px;
-  }
-`;
-
-const SkillBar = ({skillName, percent})=>{
-  return(
-    <SkillBarBlock percent={percent} skillName={skillName}>
-      <div className="info">
-        <span>{skillName}</span>
-      </div>
-      <div className="progressLine">
-        <span></span>
-      </div>
-    </SkillBarBlock>
-  )
-};
-
-SkillBar.propTypes = {
-  percent: PropTypes.string,
-}
 
 const SkillSet = ()=>{
   return(
   <SkillSetBlock>
     <div className="header">
-      <span className="skillSet">Skill Set</span>
+      <span className="skillSetTitle">Skill Set</span>
+      <span className="skillBarInfo">
+        <div className="skillBarInfoDetail">
+          <div>입문: 사용해본 경험이 있다.</div>
+          <div>초급: 디버깅할 때 에러 원인을 파악하는데 시간이 걸린다.</div>
+          <div>중급: 디버깅이 수월하다.</div>
+          <div>고급: 에러의 가능성을 줄이는 코드를 짠다.</div>
+        </div>
+        <div className="gridContainer">
+          <div>
+            <span></span>
+          </div>
+          <div>입문</div>
+          <div>초급</div>
+          <div>중급</div>
+          <div>고급</div>
+          <div>0% - 25%</div>
+          <div>25% - 50%</div>
+          <div>50% - 75%</div>
+          <div>75% - 100%</div>
+          <div><AiOutlineQuestionCircle /></div>
+        </div>
+      </span>
     </div>
     <div className="container">
       <div className="frontEnd">
+        <span>Front End</span>
         <SkillBar skillName="HTML/CSS" percent="75%"/>
         <SkillBar skillName="React/Redux" percent="50%"/>
+        <SkillBar skillName="Redux-Saga" percent="25%" />
       </div>
       <div className="backEnd">
+        <span>Back End</span>
         <SkillBar skillName="Node.js/Koa" percent="25%"/>
         <SkillBar skillName="MongoDB" percent="25%"/>
       </div>
       <div className="language">
+        <span>Language</span> 
         <SkillBar skillName="JavaScript" percent="75%"/>
         <SkillBar skillName="Python" percent="50%"/>
       </div>
